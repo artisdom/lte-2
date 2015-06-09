60705832a171bf73da78ddf1d4ee03f71b343b08
Daniel Bovensiepen <daniel@bovensiepen.net>
2015-06-09 16:33:29 +0800

    change location of logfile

--- lte_lib.rb
+++ lte_lib.rb
@@ -2,7 +2,7 @@ QC_IP = '172.20.100.1'
 
 def logit msg
   t = Time.now.strftime "%Y-%m-%d--%H-%M-%S.%L"
-  File.open("/var/log/lte_test.log", 'a') do |f|
+  File.open("/tmp/lte_test.log", 'a') do |f|
     f.puts "[#{t}];#{msg}"
   end
 end
