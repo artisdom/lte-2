751e4870da44038f8795000663d8238bcaacc676
Daniel Bovensiepen <daniel@bovensiepen.net>
2015-06-09 14:03:00 +0800

    first attempt of poking firwarell

--- receiver.rb
+++ receiver.rb
@@ -5,17 +5,25 @@ require_relative 'lte_lib'
 
 RCV_IP = ARGV[0]
 PORT = ARGV[1]
+MODE = ARGV[2]
 
 a=0
-[ RCV_IP, PORT ].each do |p|
-  raise ArgumentError.new "#{a} argument missing... (RCV_IP, PORT)" if p.nil?
+[ RCV_IP, PORT, MODE ].each do |p|
+  raise ArgumentError.new "#{a} argument missing... (RCV_IP, PORT, MODE)" if p.nil?
   a+=1
 end
+raise ArgumentError.new('wrong mode') unless MODE == 'rp' or MODE == 'qc'
 
 logit "#000;Start Receiver;#{RCV_IP};#{PORT}"
 
 socket = UDPSocket.new
 socket.bind RCV_IP, PORT
+
+if MODE == 'rp'
+  # poke firewall
+  socket.send 'poke', 0, QC_IP, PORT
+end
+
 loop do
   begin
     msg, sender_inet_addr = socket.recvfrom_nonblock(1500)
--- sender.rb
+++ sender.rb
@@ -8,18 +8,20 @@ TARGET_PORT = ARGV[1]
 PKG_SIZE = ARGV[2]
 PKG_INTERVAL = ARGV[3]
 STREAM_ID = ARGV[4]
+MODE = ARGV[5]
 
 a=0
 [
   TARGET_IP, TARGET_PORT,
   PKG_SIZE, PKG_INTERVAL, STREAM_ID
 ].each do |p|
-  raise ArgumentError.new "#{a} argument missing... (TARGET_IP, TARGET_PORT, PKG_SIZE, PKG_INTERVAL, STREAM_ID)" if p.nil?
+  raise ArgumentError.new "#{a} argument missing... (TARGET_IP, TARGET_PORT, PKG_SIZE, PKG_INTERVAL, STREAM_ID, MODE)" if p.nil?
   a+=1
 end
 if PKG_SIZE.to_i <= 24
   raise ArgumentError.new 'package size has to be bigger than 24byte'
 end
+raise ArgumentError.new('wrong mode') unless MODE == 'rp' or MODE == 'qc'
 
 logit "#100;Start Sender;#{TARGET_IP};#{TARGET_PORT};#{PKG_SIZE};#{PKG_INTERVAL}"
 socket = UDPSocket.new
@@ -27,9 +29,24 @@ interval = PKG_INTERVAL.to_f
 content = ("a" * (PKG_SIZE.to_i - 24))
 stream_id = STREAM_ID.rjust 4, '0'
 c=0
+
+if MODE == 'qc'
+  socket.bind '0.0.0.0', TARGET_PORT
+  msg, sender_inet_addr = socket.read(4)
+  if msg == 'poke'
+    target_port = sender_inet_addr[1]
+    target_ip = sender_inet_addr[2]
+  else
+    raise Exception.new 'unknown poke command'
+  end
+else
+  target_ip = TARGET_IP
+  target_port = TARGET_PORT
+end
+
 loop do
   c+=1
-  socket.send (c.to_s.rjust(20,'0') + stream_id + content), 0, TARGET_IP, TARGET_PORT
+  socket.send (c.to_s.rjust(20,'0') + stream_id + content), 0, target_ip, target_port
   logit "#101;Send UDP Telegram;#{c};#{PKG_SIZE};#{TARGET_IP};#{TARGET_PORT}"
   sleep interval
 end
