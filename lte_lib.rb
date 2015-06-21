450f8c741a73492a4243a761b7a7cc0cb869d4ac
Daniel Bovensiepen <daniel@bovensiepen.net>
2015-06-22 00:52:32 +0800

    add debug mode

--- lte_lib.rb
+++ lte_lib.rb
@@ -1,3 +1,5 @@
+$D = true
+
 QC_IP = '172.20.100.1'
 TESTRUN_CONFIG = '/tmp/testrun.json'
 TESTRUN_STATUS = '/tmp/testrun.status'
@@ -5,7 +7,11 @@ TESTRUN_PID = '/tmp/testrun.pid'
 
 def logit msg
   t = Time.now.strftime "%Y-%m-%d--%H-%M-%S.%L"
-  File.open("/tmp/lte_test.log", 'a') do |f|
-    f.puts "[#{t}];#{msg}"
+  if $D
+    puts "[#{t}];#{msg}"
+  else
+    File.open("/tmp/lte_test.log", 'a') do |f|
+      f.puts "[#{t}];#{msg}"
+    end
   end
 end
